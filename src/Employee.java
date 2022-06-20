ae2255c541d762d5b4c3636f6e17225a17f3fb1f
Lucia Galvez <lucia.galudev@gmail.com>
2022-06-20 17:51:06 +0200

    Solución del Ejercicio 34

---
+++ src/Employee.java
@@ -0,0 +1,17 @@
+
+public class Employee {
+	private float salary;
+
+	public Employee(float salary) {
+		super();
+		this.salary = salary;
+	}
+
+	public float getSalary() {
+		return salary;
+	}
+
+	public void setSalary(float salary) {
+		this.salary = salary;
+	}
+}
